269ac2ca2b94feee44f8cf6d8d7b13fb7bdb29ad
Anton <antonrozdobudko@gmail.com>
2021-08-06 15:35:43 +0300

    React. Lesson 5. Условный рендеринг

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,14 @@ import {Rating} from './Rating/Rating';
 function App() {
     return (
         <div>
-            <PageTitle title={"This is APP component"}/>
-            <PageTitle title={"My friends"}/>
-            Article 1
-            <Rating value={3}/>
             <Accordion titleValue={"Menu"} collapsed={false}/>
             <Accordion titleValue={"Students"} collapsed={true}/>
-            Article 2
-            <Rating value={0}/>
-            <Rating value={1}/>
-            <Rating value={2}/>
-            <Rating value={3}/>
-            <Rating value={4}/>
-            <Rating value={5}/>
+            <Rating value={0} />
+            <Rating value={1} />
+            <Rating value={2} />
+            <Rating value={3} />
+            <Rating value={4} />
+            <Rating value={5} />
         </div>
     );
 }
